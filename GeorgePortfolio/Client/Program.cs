b0c68f2991078758e1d5e09e7690f180a1249f64
George Wagih <George.Wagih@tera.ltd>
2025-06-21 10:29:38 +0300

    sss

--- GeorgePortfolio/Client/Program.cs
+++ GeorgePortfolio/Client/Program.cs
@@ -14,6 +14,8 @@ namespace GeorgePortfolio
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+
+
             await builder.Build().RunAsync();
         }
     }
