545f8ffef1471748192df97760c376a474810610
George Wagih <George.Wagih@tera.ltd>
2025-06-21 12:03:36 +0300

    rename to portfolio

--- GeorgePortfolio/Client/Program.cs
+++ GeorgePortfolio/Client/Program.cs
@@ -14,7 +14,6 @@ namespace GeorgePortfolio
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-
             await builder.Build().RunAsync();
         }
     }
