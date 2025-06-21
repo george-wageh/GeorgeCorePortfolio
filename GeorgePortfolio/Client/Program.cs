04d1bcb0df128f548db6f77895b96c9e93ad619f
George Wagih <George.Wagih@tera.ltd>
2025-06-21 10:57:58 +0300

    zx

--- GeorgePortfolio/Client/Program.cs
+++ GeorgePortfolio/Client/Program.cs
@@ -15,7 +15,6 @@ namespace GeorgePortfolio
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 
-
             await builder.Build().RunAsync();
         }
     }
